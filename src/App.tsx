dcbb519d8688248d7d32faab561272f3574992b7
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-18 23:20:31 +0200

    update responsive

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import PiscineLogo from "./components/PiscineLogo";
 function App() {
   return (
     <main className=" py-10 2xl:py-8 2xl:px-0 bg-[#F3F3F1] text-[#262625] font-medium overflow-x-hidden">
-      <header className="min-h-[40px] mx-auto px-3 2xl:px-0 max-w-[1440px] w-full flex flex-row justify-between items-center mb-[114px] 2xl:mb-[141px]">
+      <header className="min-h-[40px] max-w-[366px]  mx-auto 2xl:px-0 2xl:max-w-[1440px]  flex flex-row justify-between items-center mb-[114px] 2xl:mb-[141px]">
         <PiscineLogo />
         <span className="font-bold">0%</span>
         <img src={SwitchLogo} alt="" />
@@ -15,7 +15,7 @@ function App() {
       <HeroSection />
       <CardsSection />
 
-      <footer className="min-h-[40px] flex flex-row justify-between mx-auto max-w-[1440px] items-center px-3 2xl:px-0">
+      <footer className="min-h-[40px] max-w-[366px] flex flex-row justify-between mx-auto 2xl:max-w-[1440px] items-center px-3 2xl:px-0">
         <PiscineLogo />
         <span className="font-bold">100%</span>
         <span className="font-bold">@2024</span>
