998e1e25060f3cbb334a10f1259454018ad2fc3b
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-20 10:15:29 +0200

    update css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ interface FooterProps {
 
 const Footer = ({ scrollProgress }: FooterProps) => {
   return (
-    <footer className="min-h-[40px] pb-[46px] pt-[65px] 2xl:pt-[46px] 2xl:pb-[38px] w-[366px] flex flex-row justify-between mx-auto 2xl:w-[1376px] items-center">
+    <footer className="relative min-h-[40px] pb-[46px] pt-[65px] 2xl:pt-[46px] 2xl:pb-[38px] w-[366px] flex flex-row justify-between mx-auto 2xl:w-[1376px] items-center">
       <PiscineLogo />
-      <NumberTicker scrollProgress={scrollProgress} className="font-bold" />
+      <NumberTicker scrollProgress={scrollProgress} className="font-bold text-center w-full absolute" />
       <span className="font-bold">@2024</span>
     </footer>
   );
