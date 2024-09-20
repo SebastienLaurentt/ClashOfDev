f8ffd1183889ff9db7cf7bda2eeaf117b83a6892
SebastienLaurentt <slaurent.26@gmail.com>
2024-09-20 19:02:34 +0200

    improve accessibility

--- src/components/LandingSections/HeroSection.tsx
+++ src/components/LandingSections/HeroSection.tsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
       if (rect) {
         setFixedPosition({
           x: e.clientX - rect.left,
-          y: e.clientY - rect.top
+          y: e.clientY - rect.top,
         });
       }
     } else {
@@ -103,7 +103,7 @@ const HeroSection = () => {
         </div>
       )}
       <div className="flex flex-col max-w-[366px] 2xl:max-w-[1376px] mx-auto 2xl:flex-row 2xl:justify-between 2xl:items-center mb-[100px] 2xl:mb-40">
-        <div className="flex flex-col mb-10 2xl:mb-0 text-[74px] leading-[82px] 2xl:text-[115px] 2xl:leading-[123px] font-semibold text-[#262625]">
+        <div className="flex flex-col mb-10 2xl:mb-0 text-[74px] leading-[82px] 2xl:text-[115px] 2xl:leading-[123px] font-semibold text-foreground">
           <span className="2xl:hidden">
             <span className="flex flex-row items-center justify-between">
               Que le
@@ -124,7 +124,7 @@ const HeroSection = () => {
               className="2xl:ml-[28px] hidden 2xl:block 2xl:mr-[45px] h-[80px] w-[103px]"
             />{" "}
             <BlurIn
-              className="text-[74px] leading-[82px] 2xl:text-[115px] 2xl:leading-[123px] font-semibold text-[#262625] italic"
+              className="text-[74px] leading-[82px] 2xl:text-[115px] 2xl:leading-[123px] font-semibold text-foreground italic"
               word="l'emporte!"
             />
           </div>
@@ -133,11 +133,15 @@ const HeroSection = () => {
           <img src={CardHero} alt="" className="2xl:h-[356px] 2xl:w-[368px]" />
         </div>
         <div className="relative h-[356px] 2xl:w-[368px] w-[366px] ml-[26px]">
-          <img src={BlankCardHero} alt="Carte de jeu blanche avec couronne" className="" />
+          <img
+            src={BlankCardHero}
+            alt="Carte de jeu blanche avec couronne"
+            className=""
+          />
           <Cursor
             name="maislina_"
             bgColor="#F28164"
-            fontColor="text-[#FEFEFE]"
+            fontColor="text-primary-foreground"
             borderColor="#D46F55"
             classname={`absolute bottom-[150px] left-[240px] 2xl:left-[265px] ${
               animate ? "animate-cursor1" : ""
@@ -146,7 +150,7 @@ const HeroSection = () => {
           <Cursor
             name="LLCoolChris"
             bgColor="#6B93C0"
-            fontColor="text-[#FEFEFE]"
+            fontColor="text-primary-foreground"
             borderColor="#6B93C0"
             classname={`absolute bottom-[65px] 2xl:left-[235px] left-[210px] ${
               animate ? "animate-cursor2" : ""
@@ -154,7 +158,7 @@ const HeroSection = () => {
           />
           {!customCursor && fixedPosition.x === 0 && fixedPosition.y === 0 && (
             <span
-              className={`w-fit text-xs absolute 2xl:flex border-2 items-center rounded-[18.49px_1.54px_18.49px_18.49px] bottom-[35px] left-[-20px] px-3 py-1.5 text-[#FEFEFE] h-[30px] 2xl:cursor-pointer`}
+              className={`w-fit text-xs absolute 2xl:flex border-2 items-center rounded-[18.49px_1.54px_18.49px_18.49px] bottom-[35px] left-[-20px] px-3 py-1.5 text-primary-foreground h-[30px] 2xl:cursor-pointer`}
               style={{ backgroundColor: "#F588B9", borderColor: "#D6699A" }}
               onClick={(e) => {
                 if (window.innerWidth > 768) {
@@ -185,7 +189,6 @@ const HeroSection = () => {
             backgroundImage: `url(${ClashVector})`,
             backgroundRepeat: "repeat-x",
           }}
-          aria-label="Slogan de Clash of Dev : 'Attrapez les tous ! Clash of Dev"
         />
       </div>
     </div>
